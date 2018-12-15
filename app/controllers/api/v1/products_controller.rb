120d918e5c6fa4333270d1c001b38b24e29451b0
Ahmed Mamdouh <ahmed_mu.mamdouh@hotmail.com>
2018-12-15 21:35:36 +0200

    Update products_controller.rb

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,14 +3,14 @@ module Api
 		class ProductsController < ApplicationController
 		  before_action :set_product, only: [:show, :update, :destroy]
 
-		  # GET api/v1/products
+		  
 		  def index
 		    @products = Product.all
 
 		    render json: { status: "SUCCESS", data: @products}, status: :ok
 		  end
 
-		  # GET api/v1/product?query=ProductName
+		  
 		  def search_by_product_name
 		    @results = Product.text_search(params[:query])
 		    if @results.nil?
@@ -24,12 +24,12 @@ module Api
 			end
 		  end
 
-		  # GET api/v1/products/1
+		  
 		  def show
 		    render json: @product
 		  end
 
-		  # POST api/v1/products
+		  
 		  def create
 		    @product = Product.new(product_params)
 
@@ -40,7 +40,7 @@ module Api
 		    end
 		  end
 
-		  # PATCH/PUT api/v1/products/1
+		  
 		  def update
 		    if @product.update(product_params)
 		      render json: { status: "SUCCESS", data: @product}, status: :ok
@@ -49,19 +49,19 @@ module Api
 		    end
 		  end
 
-		  # DELETE api/v1/products/1
+		  
 		  def destroy
 		    @product.destroy
 		    render json: { message: "Product deleted"}
 		  end
 
 		  private
-		    # Use callbacks to share common setup or constraints between actions.
+		    
 		    def set_product
 		      @product = Product.find(params[:id])
 		    end
 
-		    # Only allow a trusted parameter "white list" through.
+		    
 		    def product_params
 		      params.require(:product).permit(:product_name)
 		    end
